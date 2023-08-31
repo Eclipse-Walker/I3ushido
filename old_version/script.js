0ba00282b3a463f77f4f963e45016eb0fd7a2370
I3ushido <phisanurat.won@msu.ac.th>
2023-09-01 00:23:02 +0700

    change font color

--- old_version/script.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".typing", {
-  strings: ["Software Engineer", "Mobile Developer", "I3ushido"],
-  typeSpeed: 110,
-  backSpeed: 90,
-  cursorChar: " _",
-  smartBackspace: true,
-  loop: true,
-});
